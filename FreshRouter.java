6420625c43fb8fc051989337c419d42fa6021f6e
Cassio Paixao <cassio.paixao@gmail.com>
2012-06-29 11:36:27 -0300

    Implementadas funções de apoio ao FRESH.

--- FreshRouter.java
+++ FreshRouter.java
@@ -1,8 +1,15 @@
 package routing;
 
+import java.util.Map;
+
+import core.Connection;
+import core.DTNHost;
 import core.Settings;
+import core.SimClock;
 
 public class FreshRouter extends ActiveRouter {
+
+	private Map<DTNHost, Double> encounterAge;
 
 	public FreshRouter(Settings s) {
 		super(s);
@@ -19,4 +26,54 @@ public class FreshRouter extends ActiveRouter {
 		return new FreshRouter(this);
 	}
 
+	/*
+	 * Retorna há quanto tempo o host encontrou o host <code>peer</code>. Se
+	 * nunca se encontraram, retorna infinito (Double.MAX_VALUE).
+	 */
+	private double prevEncounterAge(DTNHost peer) {
+
+		if (encounterAge.containsKey(peer)) {
+			Double prevEncAge = encounterAge.get(peer);
+			return SimClock.getTime() - prevEncAge.doubleValue();
+		} else {
+			return Double.MAX_VALUE;
+		}
+
+	}
+
+	/*
+	 * Encontra um host que esteve em contato com o destino há menos tempo.
+	 * Se não houver nenhum, retorna <code>null</code>.
+	 */
+	private DTNHost findNextAnchor(DTNHost dest, double time) {
+		DTNHost anchor = null;
+		double maisRecente = Double.MAX_VALUE;
+		for (Connection con : getConnections()) {
+			double tempo = prevEncounterAge(con.getOtherNode(this.getHost()));
+			if (tempo <= maisRecente) {
+				anchor = con.getOtherNode(this.getHost());
+			}
+		}
+		
+		return anchor;
+	}
+
+	@Override
+	public void changedConnection(Connection con) {
+		super.changedConnection(con);
+		
+		DTNHost peer = con.getOtherNode(this.getHost());
+		encounterAge.put(peer, SimClock.getTime());
+	}
+
+	@Override
+	/*
+	 * (non-Javadoc) Elimina do buffer as mensagens transferidas.
+	 * @see routing.ActiveRouter#transferDone(core.Connection)
+	 */
+	protected void transferDone(Connection con) {
+		/* don't leave a copy for the sender */
+		this.deleteMessage(con.getMessage().getId(), false);
+	}
+	
 }
